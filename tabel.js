206108426caff563975e320a0339a21f33e4192a
grimbaz <grim.baeke@gmail.com>
2023-03-15 12:12:31 +0100

    new base web

--- tabel.js
+++ tabel.js
@@ -1,106 +1,65 @@
-// voorbeeldgegevens van gebruikers en rollen
-var userData = [	{		naam: "John",		rollen: ["admin"]
-	},
-	{
-		naam: "Jane",
-		rollen: ["editor"]
-	},
-	{
-		naam: "Bob",
-		rollen: ["viewer"]
-	}
-];
+// Get modal element
+const modal = document.querySelector('.modal');
 
-// toevoegen van gebruikersgegevens aan de tabel
-function addUserDataToTable(userData) {
-	var tableBody = document.querySelector("#user-table tbody");
-	tableBody.innerHTML = "";
+// Get modal button
+const editBtns = document.querySelectorAll('.edit-btn');
 
-	for (var i = 0; i < userData.length; i++) {
-		var row = document.createElement("tr");
+// Get close button
+const closeBtn = document.querySelector('.close');
 
-		var nameCell = document.createElement("td");
-		var rolesCell = document.createElement("td");
-		var editDeleteCell = document.createElement("td");
-
-		var editButton = document.createElement("button");
-		editButton.textContent = "Bewerken";
-		editButton.classList.add("edit");
-
-		var deleteButton = document.createElement("button");
-		deleteButton.innerHTML = '<i class="fa fa-trash"></i>';
-		deleteButton.classList.add("delete");
-
-		nameCell.appendChild(document.createTextNode(userData[i].naam));
-		row.appendChild(nameCell);
-
-		rolesCell.appendChild(document.createTextNode(userData[i].rollen.join(", ")));
-		row.appendChild(rolesCell);
-
-		editDeleteCell.appendChild(editButton);
-		editDeleteCell.appendChild(deleteButton);
-		row.appendChild(editDeleteCell);
-
-		tableBody.appendChild(row);
-	}
+// Function to open modal
+function openModal() {
+    modal.style.display = 'block';
 }
 
-// gebruikersgegevens laden wanneer de pagina wordt geladen
-window.addEventListener("load", function() {
-	addUserDataToTable(userData);
+// Function to close modal
+function closeModal() {
+    modal.style.display = 'none';
+}
+
+// Event listener for edit button click
+editBtns.forEach(function(editBtn) {
+    editBtn.addEventListener('click', function() {
+        openModal();
+    });
 });
 
-// toevoegen van nieuwe gebruiker
-var addUserButton = document.querySelector("nav a:nth-child(3)");
-
-addUserButton.addEventListener("click", function() {
-	var naam = prompt("Voer de naam van de nieuwe gebruiker in:");
-	if (naam) {
-		var newUser = {
-			naam: naam,
-			rollen: []
-		};
-		userData.push(newUser);
-		addUserDataToTable(userData);
-	}
+// Event listener for close button click
+closeBtn.addEventListener('click', function() {
+    closeModal();
 });
 
-// bewerken van gebruiker
-var tableBody = document.querySelector("#user-table tbody");
+// Event listener for outside click
+window.addEventListener('click', function(e) {
+    if (e.target == modal) {
+        closeModal();
+    }
+});
 
-tableBody.addEventListener("click", function(event) {
-	if (event.target.classList.contains("edit")) {
-		var editButton = event.target;
-		var deleteButton = editButton.nextElementSibling;
-		var editDeleteCell = editButton.parentNode;
-		var rolesCell = editDeleteCell.previousElementSibling;
-		var nameCell = rolesCell.previousElementSibling;
-		var naam = nameCell.textContent;
-		var rollen = rolesCell.textContent.split(", ");
+//First, add an event listener to the create button in your JavaScript code
+const createBtn = document.querySelector('.createBtn');
+createBtn.addEventListener('click', createUser);
 
-		var nieuweRollen = prompt("Voer de nieuwe rollen voor " + naam + " in, gescheiden door komma's:");
-		if (nieuweRollen) {
-			rollen = nieuweRollen.split(", ");
-			userData.forEach(function(user) {
-				if (user.naam === naam) {
-					user.rollen = rollen;
-				}
-			});
-			rolesCell.textContent = rollen.join(", ");
-		}
-	} else if (event.target.classList.contains("delete")) {
-		var deleteButton = event.target;
-		var editButton = deleteButton.previousElementSibling;
-		var editDeleteCell = deleteButton.parentNode;
-		var rolesCell = editDeleteCell.previousElementSibling;
-		var nameCell = rolesCell.previousElementSibling;
-		var naam = nameCell.textContent;
-		var confirmation = confirm("Weet u zeker dat u " + naam + " wilt verwijderen?");		
-        if (confirmation) {
-            userData = userData.filter(function(user) {
-                return user.naam !== naam;
-            });
-            addUserDataToTable(userData);
-        }
-        }
-        });
+function createUser() {
+    // Get the form input values
+    const name = document.querySelector('#name').value;
+    const role = document.querySelector('#role').value;
+    
+    // Create a new table row with the user data
+    const newRow = document.createElement('tr');
+    newRow.innerHTML = `
+      <td>${name}</td>
+      <td>${role}</td>
+      <td class="editBtn"><i class="fas fa-edit"></i></td>
+      <td class="deleteBtn"><i class="fas fa-trash-alt"></i></td>
+    `;
+    
+    // Add the new row to the table body
+    const tableBody = document.querySelector('tbody');
+    tableBody.appendChild(newRow);
+    
+    // Reset the form
+    document.querySelector('#name').value = '';
+    document.querySelector('#role').value = '';
+  }
+  
