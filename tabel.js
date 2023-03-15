8038c56dd0acebd093c1b415b06b8ee8da15519b
grimbaz <grim.baeke@gmail.com>
2023-03-15 14:40:33 +0100

    the save button works this is web v2

--- tabel.js
+++ tabel.js
@@ -19,3 +19,27 @@ home.addEventListener("click", function() {
 
     window.location.href = url;
 });
+
+//the save button save the data in the list
+const userTableBody = document.querySelector("#userTableBody");
+
+document.querySelector(".saveBtn").addEventListener("click", function(event) {
+  event.preventDefault(); // prevent the form from actually submitting
+  
+  const username = document.querySelector("#username").value;
+  const role = document.querySelector("#role").value;
+  
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>${username}</td>
+    <td>${role}</td>
+    <td><button class="editBtn">&#x270E;</button></td>
+    <td><button class="deleteBtn">&#x1F5D1;</button></td>
+  `;
+  
+  userTableBody.appendChild(newRow);
+  
+  // close the modal
+  const modal = document.querySelector("#myModal");
+  modal.style.display = "none";
+});
