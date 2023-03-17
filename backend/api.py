5a9dd15c4a628bb7b25a5bd5912b36d7171b9966
grimbaz <grim.baeke@gmail.com>
2023-03-17 14:01:36 +0100

    + api exemple

--- backend/api.py
+++ backend/api.py
@@ -1,45 +1,46 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import json
 from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-users = [
-    {'name': 'User 1', 'roles': ['Role 1', 'Role 2']},
-    {'name': 'User 2', 'roles': ['Role 3', 'Role 4', 'Role 5']},
-    {'name': 'User 3', 'roles': ['Role 2', 'Role 5', 'Role 8']},
-]
+# Define a list of users for testing purposes
+users = [{'username': 'user1', 'roles': ['Role 1', 'Role 2']},
+         {'username': 'user2', 'roles': ['Role 3', 'Role 4']}]
 
 @app.route('/users', methods=['GET'])
 def get_users():
     return jsonify(users)
 
+@app.route('/users/<username>', methods=['GET'])
+def get_user(username):
+    for user in users:
+        if user['username'] == username:
+            return jsonify(user)
+    return jsonify({'error': 'User not found'})
+
+@app.route('/users/<username>', methods=['PUT'])
+def update_user(username):
+    new_user = json.loads(request.data)
+    for user in users:
+        if user['username'] == username:
+            user['roles'] = new_user['roles']
+            return jsonify(user)
+    return jsonify({'error': 'User not found'})
+
+@app.route('/users/<username>', methods=['DELETE'])
+def delete_user(username):
+    for user in users:
+        if user['username'] == username:
+            users.remove(user)
+            return jsonify(user)
+    return jsonify({'error': 'User not found'})
+
 @app.route('/users', methods=['POST'])
-def add_user():
-    new_user = request.get_json()
+def create_user():
+    new_user = json.loads(request.data)
     users.append(new_user)
     return jsonify(new_user)
 
-@app.route('/users/<name>', methods=['GET'])
-def get_user(name):
-    user = next((user for user in users if user['name'] == name), None)
-    if user:
-        return jsonify(user)
-    else:
-        return 'User not found', 404
-
-@app.route('/users/<name>', methods=['PUT'])
-def update_user(name):
-    user = next((user for user in users if user['name'] == name), None)
-    if user:
-        user.update(request.get_json())
-        return jsonify(user)
-    else:
-        return 'User not found', 404
-
-@app.route('/users/<name>', methods=['DELETE'])
-def delete_user(name):
-    global users
-    users = [user for user in users if user['name'] != name]
-    return '', 204
-
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
