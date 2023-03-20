4670c91413e8e91959d19282aaf5d5ea9a2bf7d4
grimbaz <grim.baeke@gmail.com>
2023-03-20 09:53:01 +0100

    progress api

--- backend/app.py
+++ backend/app.py
@@ -4,18 +4,30 @@ app = Flask(__name__)
 users = [
     {
         "name": "user",
-        "roles": ["role1","role2"]
+        "roles": ["role1", "role2"]
     }
-        ]
+]
+
 
 @app.get("/users")
 def get_users():
-    return{"users" : users}
+    return {"users": users}
 
-@app.post("/user")
+
+@app.post("/users")
 def create_user():
     request_data = request.get_json()
-    new_user = {"name": request_data["name"]}
+    new_user = {"name": request_data["name"], "roles": []}
     users.append(new_user)
-    return {"status": "OK"}
+    return new_user, 201
 
+
+@app.post("/users/<string:name>/roles")
+def create_roles(name):
+    request_data = request.get_json()
+    for user in users:
+        if user["name"] == name:
+            new_role = {"name": request_data["name"]}
+            user["roles"].append(new_role)
+            return new_role, 201
+    return {"message": "user not found"}, 404
