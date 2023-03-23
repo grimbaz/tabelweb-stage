593cc533e291941ed0ff03e8666bfeeb765e7221
grimbaz <grim.baeke@gmail.com>
2023-03-23 09:14:07 +0100

    formate doc

--- frontend/tabel.js
+++ frontend/tabel.js
@@ -1,14 +1,12 @@
-
 // home button
 var home = document.getElementById("home");
 
 // Add a click event listener to the button
-home.addEventListener("click", function() {
+home.addEventListener("click", function () {
+  // Set the URL of the new page
+  var url = "tabel.html";
 
-    // Set the URL of the new page
-    var url = "tabel.html";
-
-    window.location.href = url;
+  window.location.href = url;
 });
 // the end!!!!!!! of making the home
 
@@ -16,18 +14,17 @@ home.addEventListener("click", function() {
 const createBtn = document.getElementById("createBtn");
 
 // Add a click event listener to the "Create" button
-createBtn.addEventListener("click", function(event) {
+createBtn.addEventListener("click", function (event) {
   // Show the modal popup window
   const modal = document.getElementById("myModal");
   modal.style.display = "block";
 });
 
-
 //roles menu
 // Add event listener for role button click in create modal window
 const roleBtns = document.querySelectorAll(".roleBtn");
 roleBtns.forEach((btn) => {
-  btn.addEventListener("click", function() {
+  btn.addEventListener("click", function () {
     if (btn.classList.contains("selected")) {
       btn.classList.remove("selected");
       btn.style.backgroundColor = "red";
@@ -40,14 +37,14 @@ roleBtns.forEach((btn) => {
 
 // Add event listener for close button click in create modal window
 const closeBtn = document.querySelector(".close");
-closeBtn.addEventListener("click", function() {
+closeBtn.addEventListener("click", function () {
   const modal = document.querySelector("#myModal");
   modal.style.display = "none";
 });
 
 // Add event listener for submit button click in create modal window
 const saveBtn = document.querySelector(".saveBtn");
-saveBtn.addEventListener("click", function() {
+saveBtn.addEventListener("click", function () {
   const username = document.querySelector("#username").value;
   const selectedRoles = document.querySelectorAll(".selected");
   const roles = Array.from(selectedRoles).map((role) => role.dataset.value);
@@ -55,20 +52,18 @@ saveBtn.addEventListener("click", function() {
   console.log("Selected roles: ", roles);
 });
 
-
 //end!!!! roles menu
 
 //the save button save the data in the list
 
-saveBtn.addEventListener("click", function(event) {
-  
+saveBtn.addEventListener("click", function (event) {
   event.preventDefault();
 
   const modal = document.querySelector("#myModal");
   const username = document.querySelector("#username").value;
   const selectedRoles = document.querySelectorAll(".roleBtn.selected");
   const roles = [];
-  selectedRoles.forEach(function(role) {
+  selectedRoles.forEach(function (role) {
     roles.push(role.innerText);
   });
 
@@ -108,15 +103,14 @@ saveBtn.addEventListener("click", function(event) {
   modal.style.display = "none";
 });
 
-
 //the end!!!! of the save button save the data in the list
 
 //the cross to close the creat window
-const closecreate = document.querySelector('.close');
-const modal = document.getElementById('myModal');
+const closecreate = document.querySelector(".close");
+const modal = document.getElementById("myModal");
 
-closecreate.addEventListener('click', () => {
-  modal.style.display = 'none';
+closecreate.addEventListener("click", () => {
+  modal.style.display = "none";
 });
 //the end!!!!!! of the cross to close the creat window
 
@@ -125,7 +119,7 @@ closecreate.addEventListener('click', () => {
 const tabledel = document.getElementById("userTableBody");
 
 // Add a click event listener to the table body
-tabledel.addEventListener("click", function(event) {
+tabledel.addEventListener("click", function (event) {
   // Check if the clicked element is a delete button
   if (event.target.classList.contains("deleteBtn")) {
     // Get the parent row of the clicked button
@@ -138,64 +132,65 @@ tabledel.addEventListener("click", function(event) {
 // end!!!!! of delete buttons
 
 //the edit buttons
+$(document).ready(function () {
+  $(".editBtn").click(function (event) {
+    event.preventDefault();
+    $(".editRoleBtn").removeClass("green");
+    // Get the index of the row being edited
+    var rowIndex = $(this).closest("tr").index();
 
-  //edit save button
-  $(document).ready(function(){
-    $(".editBtn").click(function(event){
-      event.preventDefault();
-      $(".editRoleBtn").removeClass("green");
-      // Get the index of the row being edited
-      var rowIndex = $(this).closest("tr").index();
-  
-      // Get the name from the corresponding row
-      var name = $("#userTableBody tr").eq(rowIndex).find(".name").text();
-      
-      // Set the name in the edit modal
-      $("#editName").val(name);
-      
-  
-      // Show the edit modal
-      $("#editModal").css("display", "block");
-  
-      // Get the roles from the corresponding row
-      var roles = $("#userTableBody tr").eq(rowIndex).find("td:eq(1)").text().split(", ");
-  
-      // Loop through the roles and add the "green" class to the relevant buttons
-      $(".editRoleBtn").each(function() {
-        var role = $(this).data("value");
-        if (roles.indexOf(role) !== -1) {
-          $(this).addClass("green");
-        }
-      });  
-  
-      // Remove the click event handler for the Save button
-      $(".saveEditBtn").off("click");
-      //make roles green or red
-      $(document).ready(function(){
-        $(".editRoleBtn").click(function(event){
-          event.preventDefault();
-          $(this).toggleClass("green");
-        });
-      });
+    // Get the name from the corresponding row
+    var name = $("#userTableBody tr").eq(rowIndex).find(".name").text();
 
-      $(".close").click(function(){
-        $("#editModal").css("display", "none");
-      });
-      // Save the edited name and roles when the Save button is clicked
-      $(".saveEditBtn").click(function(event){
+    // Set the name in the edit modal
+    $("#editName").val(name);
+
+    // Show the edit modal
+    $("#editModal").css("display", "block");
+
+    // Get the roles from the corresponding row
+    var roles = $("#userTableBody tr")
+      .eq(rowIndex)
+      .find("td:eq(1)")
+      .text()
+      .split(", ");
+
+    // Loop through the roles and add the "green" class to the relevant buttons
+    $(".editRoleBtn").each(function () {
+      var role = $(this).data("value");
+      if (roles.indexOf(role) !== -1) {
+        $(this).addClass("green");
+      }
+    });
+
+    // Remove the click event handler for the Save button
+    $(".saveEditBtn").off("click");
+    //make roles green or red
+    $(document).ready(function () {
+      $(".editRoleBtn").click(function (event) {
         event.preventDefault();
-        
-        // Get the new name from the edit modal
-        var newName = $("#editName").val();
-        var newroles = $(".green").text();
-        // Update the corresponding row with the new name
-        
-        $("#userTableBody tr").eq(rowIndex).find(".name").text(newName);
-        $("#userTableBody tr").eq(rowIndex).find(".role").text(newroles);
-        // Hide the edit modal
-        $("#editModal").css("display", "none");
+        $(this).toggleClass("green");
       });
     });
+
+    $(".close").click(function () {
+      $("#editModal").css("display", "none");
+    });
+    // Save the edited name and roles when the Save button is clicked
+    $(".saveEditBtn").click(function (event) {
+      event.preventDefault();
+
+      // Get the new name from the edit modal
+      var newName = $("#editName").val();
+      var newroles = $(".green").text();
+      // Update the corresponding row with the new name
+
+      $("#userTableBody tr").eq(rowIndex).find(".name").text(newName);
+      $("#userTableBody tr").eq(rowIndex).find(".role").text(newroles);
+      // Hide the edit modal
+      $("#editModal").css("display", "none");
+    });
   });
-  
+});
+
 //end!!!!!!!!! of the edit buttons
